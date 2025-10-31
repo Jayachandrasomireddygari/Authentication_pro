95d4a0b5b5b96a703f73166b5e85bbeed49bacbc
jayachandrasomireddygari <jayachandra.somireddygari>
2025-10-31 13:24:44 +0530

    new commit

--- auth_pro/auth_pro/settings.py
+++ auth_pro/auth_pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d(de3ixn6-5t^q^x*shyxanx6n8%g@m5*v347j4@mot)qg1s5&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
